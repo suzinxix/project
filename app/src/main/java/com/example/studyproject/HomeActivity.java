5cba320befd52935bb2aa33951b9f4ccb118ba9e
suzin <suzinxix@gmail.com>
2021-02-02 14:56:07 +0900

    홈화면

---
+++ app/src/main/java/com/example/studyproject/HomeActivity.java
@@ -0,0 +1,50 @@
+package com.example.studyproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    HomeFragment fragment_home;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        fragment_home = new HomeFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment_home).commit();
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.tab1:
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.container, fragment_home).commit();
+
+                        return true;
+                    case R.id.tab2:
+
+                        return true;
+                    case R.id.tab3:
+
+
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/studyproject/TodoFragment.java
@@ -0,0 +1,56 @@
+package com.example.studyproject;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class TodoFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private TodoAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<Todo> todo;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v =  inflater.inflate(R.layout.fragment_todolist, container, false);
+        Context context = v.getContext();
+        recyclerView = (RecyclerView) v.findViewById(R.id.recyclerview_todo);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        adapter = new TodoAdapter();
+        recyclerView.setAdapter(adapter);
+
+        return v;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        prepareData();
+    }
+    private void prepareData() {
+
+        adapter.addItem(new Todo(1,"7시에 인나기"));
+        adapter.addItem(new Todo(2,"아침에 일어나서 물마시기"));
+        adapter.addItem(new Todo(3,"식물에 물주기"));
+    }
+
+
+
+
+
+
+}
