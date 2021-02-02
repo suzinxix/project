1982d61847683e33e51b359d18ef719a67eab430
hap6v6 <hap6v6@sookmyung.ac.kr>
2021-02-02 19:31:46 +0900

    툴바 외

--- app/src/main/java/com/example/studyproject/MainActivity.java
+++ app/src/main/java/com/example/studyproject/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.studyproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-// 이얍
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       //꿀터디 메인화면
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLogin = new Intent(MainActivity.this, MyStudyRoom.class); //수정
+                startActivity(intentLogin);
+            }
+        });
     }
 }
