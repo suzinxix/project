871a9bc73ed1721cb8707ec60814510df3b1901e
hap6v6 <hap6v6@sookmyung.ac.kr>
2021-02-04 17:30:46 +0900

    툴바 모양만 성공

--- app/src/main/java/com/example/studyproject/MyStudy.java
+++ app/src/main/java/com/example/studyproject/MyStudy.java
@@ -5,57 +5,40 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+
 public class MyStudy extends Fragment {
     PopupMenu popupMenu;
     private androidx.appcompat.widget.Toolbar toolbar;
     ImageButton ibt_popup, ibt_info;
-
-
-
-//        popupMenu = new PopupMenu(this, null, Gravity.END, 0, R.style.MyPopupMenu);
-//        popupMenu.getMenu( ).add( 0, 0, 0, "리스트 첫번째" );
-//        popupMenu.getMenu( ).add( 0, 1, 0, "리스트 두번째" );
-//        popupMenu.getMenu( ).add( 0, 2, 0, "리스트 세번째" );
-//        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-//            @Override
-//            public boolean onMenuItemClick(MenuItem item) {
-//                switch (item.getItemId( )) {
-//                    case 0: //리스트 첫번째 클릭시 호출
-//                        break;
-//                    case 1: //리스트 두번째 클릭시 호출
-//                        break;
-//                    case 2: //리스트 세번째 클릭시 호출
-//                        break;
-//                }
-//
-//                return false;
-//            }
-//        });
-
-
+    MyStudyRoom studyRoom;
+    TextView textViewtextView;
+    View viewToolbar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_mystudy, container, false);
 
-        toolbar = (Toolbar) view.findViewById(R.id.toolbar_mystudy);
-        MyStudyRoom studyRoom = (MyStudyRoom) getActivity();
+        toolbar = (Toolbar) view.findViewById(R.id.include_toolbar);
+        studyRoom = (MyStudyRoom) getActivity();
         studyRoom.setSupportActionBar(toolbar);
         ActionBar actionBar = ((MyStudyRoom) getActivity()).getSupportActionBar();
         actionBar.setDisplayShowCustomEnabled(true);
         actionBar.setDisplayShowTitleEnabled(false);
 
-        View viewToolbar = getActivity().getLayoutInflater().inflate(R.layout.actionbar_mystudy, null);
-        ibt_popup = (ImageButton)viewToolbar.findViewById(R.id.imageButtonMenu);
+        viewToolbar = getActivity().getLayoutInflater().inflate(R.layout.fragment_mystudy, null);
+//        ibt_popup = (ImageButton)viewToolbar.findViewById(R.id.imageButtonMenu);
 //        ibt_popup.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
@@ -91,11 +74,21 @@ public class MyStudy extends Fragment {
         ibt_info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Toast.makeText(studyRoom, "hello world!", Toast.LENGTH_SHORT).show();
+                textViewtextView.setText("Toolbar 버튼 클릭");
             }
         });
 
-        actionBar.setCustomView(viewToolbar, new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.CENTER));
+        textViewtextView = (TextView)view.findViewById(R.id.textViewtextView);
+        Button buttonbutton = (Button)view.findViewById(R.id.buttonbutton);
+        buttonbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewtextView.setText("Fragement 버튼 클릭");
+            }
+        });
+
+//        actionBar.setCustomView(viewToolbar, new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.CENTER));
         return view;
     }
 }
