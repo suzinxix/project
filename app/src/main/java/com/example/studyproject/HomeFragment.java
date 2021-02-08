a27d12d875a4c863fddf6b108d42a702f190d70d
suzinxix <suzinxix@gmail.com>
2021-02-09 00:59:28 +0900

    툴바 메뉴추가

--- app/src/main/java/com/example/studyproject/HomeFragment.java
+++ app/src/main/java/com/example/studyproject/HomeFragment.java
@@ -1,14 +1,19 @@
 package com.example.studyproject;
 
+import android.app.VoiceInteractor;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
@@ -21,21 +26,44 @@ public class HomeFragment extends Fragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
+        setHasOptionsMenu(true);
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // 툴바 추가
+        toolbar_home = (Toolbar) view.findViewById(R.id.toolbarHome);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar_home);
+
         // 로그아웃 구현
-        bt_logout = (Button)view.findViewById(R.id.bt_logout);
+        bt_logout = (Button) view.findViewById(R.id.bt_logout);
         bt_logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(getActivity(),LoginActivity.class));
+                startActivity(new Intent(getActivity(), LoginActivity.class));
             }
         });
+
         return view;
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // TODO Add your menu entries here
+        inflater.inflate(R.menu.menu_mystudy, menu) ;
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.weekly:
+                // Do onlick on menu action here
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
 //        //툴바(액션바) 추가
-//        toolbar_home = findViewById(R.id.toolbarHome);
+//        toolbar_home = (Toolbar)view.findViewById(R.id.toolbarHome);
 //        setSupportActionBar(toolbar_home);
 //        ActionBar actionBar = getSupportActionBar();
 //        actionBar.setDisplayShowCustomEnabled(false);
@@ -47,5 +75,5 @@ public class HomeFragment extends Fragment{
 //
 //        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
 //        actionBar.setCustomView(actionbarView, layout);
-    }
+
 }
