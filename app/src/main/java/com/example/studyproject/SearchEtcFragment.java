9d882993c096c73948ba8f775a933b59c9102e0d
jelly <nayapark0814@gmail.com>
2021-02-23 02:14:40 +0900

    스터디룸 db 리사이클러뷰 카테고리별 (완료)

--- app/src/main/java/com/example/studyproject/SearchEtcFragment.java
+++ app/src/main/java/com/example/studyproject/SearchEtcFragment.java
@@ -4,17 +4,106 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class SearchEtcFragment extends Fragment {
+    private RecyclerView recview_6;
+    private View ContactsView;
+    private DatabaseReference ContactsRef, RoomRef;
+    Query query = FirebaseDatabase.getInstance().getReference("study_rooms")
+            .orderByChild("roomcategory")
+            .equalTo("etc");
+
+    public SearchEtcFragment() {
+
+    }
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_search_etc, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ContactsView = inflater.inflate(R.layout.fragment_search_etc, container, false);
+        recview_6 = (RecyclerView) ContactsView.findViewById(R.id.recview_6);
+        recview_6.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        return rootView;
+        ContactsRef = FirebaseDatabase.getInstance().getReference().child("study_rooms");
+        RoomRef = FirebaseDatabase.getInstance().getReference().child("study_rooms");
+        return ContactsView;
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<MakeRoomDB> options =
+                new FirebaseRecyclerOptions.Builder<MakeRoomDB>()
+                        .setQuery(query, MakeRoomDB.class) // 노드 데이터 읽어오기
+                        .build();
+
+        FirebaseRecyclerAdapter<MakeRoomDB, SearchEtcFragment.ContactsViewHolder> adapter = new FirebaseRecyclerAdapter<MakeRoomDB, SearchEtcFragment.ContactsViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull final SearchEtcFragment.ContactsViewHolder holder, int position, @NonNull MakeRoomDB model) {
+                String roomId = getRef(position).getKey(); // 리사이클러뷰
+
+                RoomRef.child(roomId).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.hasChild("study_rooms")) {
+                            String room_name = snapshot.child("roomname").getValue().toString();
+                            String room_info = snapshot.child("roominfo").getValue().toString();
+
+                            holder.roomname.setText(room_name);
+                            holder.roominfo.setText(room_info);
+                        } else {
+                            String room_name = snapshot.child("roomname").getValue().toString();
+                            String room_info = snapshot.child("roominfo").getValue().toString();
+
+                            holder.roomname.setText(room_name);
+                            holder.roominfo.setText(room_info);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+            }
+
+            @NonNull
+            @Override
+            public SearchEtcFragment.ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+                View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.singlerowdesign, viewGroup, false);
+                SearchEtcFragment.ContactsViewHolder viewHolder = new SearchEtcFragment.ContactsViewHolder(view);
+                return viewHolder;
+            }
+        };
+        recview_6.setAdapter(adapter);
+        adapter.startListening();
+    }
+    public static class ContactsViewHolder extends RecyclerView.ViewHolder {
+        TextView roomname, roominfo;
+
+        public ContactsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            roomname = itemView.findViewById(R.id.text_1);
+            roominfo = itemView.findViewById(R.id.text_2);
+        }
     }
 }
--- app/src/main/java/com/example/studyproject/SearchHobbyFragment.java
+++ app/src/main/java/com/example/studyproject/SearchHobbyFragment.java
@@ -4,17 +4,106 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class SearchHobbyFragment extends Fragment {
+    private RecyclerView recview_4;
+    private View ContactsView;
+    private DatabaseReference ContactsRef, RoomRef;
+    Query query = FirebaseDatabase.getInstance().getReference("study_rooms")
+            .orderByChild("roomcategory")
+            .equalTo("hobby");
+
+    public SearchHobbyFragment() {
+
+    }
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_search_hobby, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ContactsView = inflater.inflate(R.layout.fragment_search_hobby, container, false);
+        recview_4 = (RecyclerView) ContactsView.findViewById(R.id.recview_4);
+        recview_4.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        return rootView;
+        ContactsRef = FirebaseDatabase.getInstance().getReference().child("study_rooms");
+        RoomRef = FirebaseDatabase.getInstance().getReference().child("study_rooms");
+        return ContactsView;
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<MakeRoomDB> options =
+                new FirebaseRecyclerOptions.Builder<MakeRoomDB>()
+                        .setQuery(query, MakeRoomDB.class) // 노드 데이터 읽어오기
+                        .build();
+
+        FirebaseRecyclerAdapter<MakeRoomDB, SearchHobbyFragment.ContactsViewHolder> adapter = new FirebaseRecyclerAdapter<MakeRoomDB, SearchHobbyFragment.ContactsViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull final SearchHobbyFragment.ContactsViewHolder holder, int position, @NonNull MakeRoomDB model) {
+                String roomId = getRef(position).getKey(); // 리사이클러뷰
+
+                RoomRef.child(roomId).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.hasChild("study_rooms")) {
+                            String room_name = snapshot.child("roomname").getValue().toString();
+                            String room_info = snapshot.child("roominfo").getValue().toString();
+
+                            holder.roomname.setText(room_name);
+                            holder.roominfo.setText(room_info);
+                        } else {
+                            String room_name = snapshot.child("roomname").getValue().toString();
+                            String room_info = snapshot.child("roominfo").getValue().toString();
+
+                            holder.roomname.setText(room_name);
+                            holder.roominfo.setText(room_info);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+            }
+
+            @NonNull
+            @Override
+            public SearchHobbyFragment.ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+                View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.singlerowdesign, viewGroup, false);
+                SearchHobbyFragment.ContactsViewHolder viewHolder = new SearchHobbyFragment.ContactsViewHolder(view);
+                return viewHolder;
+            }
+        };
+        recview_4.setAdapter(adapter);
+        adapter.startListening();
+    }
+    public static class ContactsViewHolder extends RecyclerView.ViewHolder {
+        TextView roomname, roominfo;
+
+        public ContactsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            roomname = itemView.findViewById(R.id.text_1);
+            roominfo = itemView.findViewById(R.id.text_2);
+        }
     }
 }
