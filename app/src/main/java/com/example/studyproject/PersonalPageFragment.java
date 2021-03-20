4febd955df0018d845ff2b188ca92036dc69e3ca
hap6v6 <hap6v6@sookmyung.ac.kr>
2021-03-20 15:59:53 +0900

    변경없음

--- app/src/main/java/com/example/studyproject/PersonalPageFragment.java
+++ app/src/main/java/com/example/studyproject/PersonalPageFragment.java
@@ -25,26 +25,27 @@ public class PersonalPageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_personal, container, false);
         //personalName = (EditText)view.findViewById(R.id.et_pname);
-        personalNick = (EditText)view.findViewById(R.id.et_pnick);
-        bt_personal = (Button)view.findViewById(R.id.bt_save);
-        tv_pname = (TextView) view.findViewById(R.id.tv_pname);
+        //personalNick = (EditText)view.findViewById(R.id.et_pnick);
+       // bt_personal = (Button)view.findViewById(R.id.bt_save);
+        //tv_pname = (TextView) view.findViewById(R.id.tv_pname);
         tv_pnick = (TextView) view.findViewById(R.id.tv_pnick);
-        bt_profileEdit = view.findViewById(R.id.profileEdit);
+        bt_profileEdit = view.findViewById(R.id.bt_profileEdit);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             myname = user.getDisplayName();
             // Check if user's email is verified
+            tv_pnick.setText("닉네임: "+myname);
         }
 
-        tv_pname.setText("이름: "+myname);
+//        tv_pname.setText("이름: "+myname);
+//        bt_personal.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                personalNick.setText(personalNick.getText());
+//            }
+//        });
 
-        bt_personal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                personalNick.setText(personalNick.getText());
-            }
-        });
         bt_profileEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
